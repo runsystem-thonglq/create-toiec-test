cb9da4b906462d354d9f22fc4a442119f8418c31
thonglq <thonglq@runsystem.net>
2025-11-11 22:55:42 +0700

    add

--- src/utils/axios.ts
+++ src/utils/axios.ts
@@ -1,7 +1,8 @@
 import axios, { AxiosInstance } from "axios";
 
 const axiosInstance: AxiosInstance = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_URL || "http://localhost:3001",
+  baseURL:
+    process.env.NEXT_PUBLIC_API_URL || "https://www.thongledeptrai.click/api",
   headers: {
     "Content-Type": "application/json",
   },
